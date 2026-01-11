fe4fad54fd3a903151d7e2c2fad43592e5262b79
BRIAN-cloud-cyber <omondiomimo@gmail.com>
2026-01-11 13:07:49 -0500

    profileCard

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,20 +1,28 @@
  
-import welcomeMessage from './components/WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() 
    { return (
     <div> 
-      <welcomeMessage />
+      <WelcomeMessage />
       <Header />
       <MainContent />
       <Footer />
-    
-    </div>
-    
-   )
+      
+    <UserProfile
+      name="Alice"
+      age="25" 
+       
+      bio="Loves hiking and photography"
+       
+      />
+    </div> 
+    );
+
 
    }
   export default App;
