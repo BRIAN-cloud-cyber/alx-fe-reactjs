d78f88d18d2fb4fea30a256e2824c7e3f49433f2
BRIAN-cloud-cyber <omondiomimo@gmail.com>
2026-01-11 12:35:07 -0500

    components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,19 @@
  
 import welcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() 
    { return (
-    <welcomeMessage />
+    <div> 
+      <welcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+    
+    </div>
+    
    )
 
    }
---
+++ alx-react-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header (){
+    return(
+        <div>
+            <header>My favorite Cities</header>
+        </div>
+    )
+}
+
+export default Header;
