e1739c71abba26f3d891e2ce711f891b3315a504
BRIAN-cloud-cyber <omondiomimo@gmail.com>
2026-01-16 12:40:06 -0500

    alx-fe-new-app

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer style={{backgroundColor:'brown', color:'white',
+                textAlign:'center', padding:'10px', marginTop:'20px',
+                position:'fixed', bottom:'0', width:'100%',
+                height:'50px'
+            }}>
+            <p>&Copy; 2023 City Lovers</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header (){
+    return(
+        <div>
+            <header style={{backgroundColor:'navy', color:'white',
+                textAlign:'center'
+            }}>
+                My Favorite Cities
+
+                
+            </header>
+        </div>
+    )
+}
+
+export default Header;
