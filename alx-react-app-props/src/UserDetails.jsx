4f67a5473de909ae05ca46d0d8dc5c9ca3b8f0c4
BRIAN-cloud-cyber <omondiomimo@gmail.com>
2026-01-17 21:00:10 -0500

    useContext

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,15 +1,15 @@
 import { useContext } from "react";
-import UserContent from "./ UserContext";
+import UserContext from "./UserContext";
 
+function UserDetails() {
+  const userData = useContext(UserContext);
 
- const UserDetails = () => {
-  const userData = useContext(UserContent);
   return (
     <div>
       <p>Name: {userData.name}</p>
       <p>Email: {userData.email}</p>
     </div>
   );
-};
+}
 
 export default UserDetails;
---
+++ alx-react-app-props/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header (){
+    return(
+        <div>
+            <header>My Favorite Cities</header>
+        </div>
+    )
+}
+
+export default Header;
